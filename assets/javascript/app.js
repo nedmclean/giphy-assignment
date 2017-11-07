415c3943576854f6e1e0380fc8078385bd4ed753
Mona Lisa <SwagwardDaniels@Edwards-MacBook-Pro-2.local>
2017-11-06 16:45:40 -0800

    html, css, ajax

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  
+  
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+  $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+      console.log(response);
+    });
