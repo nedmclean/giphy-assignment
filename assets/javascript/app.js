9568813a4fc07657b3492cf7b3d1f8b446cf919d
Mona Lisa <SwagwardDaniels@Edwards-MacBook-Pro-2.local>
2017-11-06 21:02:18 -0800

    updated js, some edits

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,66 @@
-$(document).ready(function(){
+$("button").on("click", function(){
 
- 
 
 
 var searchButtons = ["octopus", "squid", "cuttlefish"];
 var animateUrl;
 
+var animal = $(this).attr("data-animal");
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).done(function(response) {
+        var results = response.data;
 
+        for (var i = 0; i <results.length; i++) {
+          var gifDiv = $("<div class='item'>");
 
-var searchTerm = $(this).attr("#custom-search-input");
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=c3BcQjpHG5GUv3Ft5llF3ztZI2fNEc0J&limit=5";
+          var rating = results[i].rating;
 
-  $.ajax({
-      url: queryURL,
-      method: 'GET'
-    }).done(function(response) {
-      console.log(response);
-    
-      for (i = 0; i < response.length; i++){
-        var newDiv = $("<div>");
-        var image = $("<img>");
-        var imageURL = response.data[i].images.fixed_height_still.url;
-        animateUrl =  response.data[i].images.fixed_height.url;
-        console.log(imageURL);
-        newDiv.addclass("gifDiv");
-        newImage.attr("src", imageURL);
-        newDiv.html(image);
-        $("#gifs").prepend(newDiv)
-      }
+          var p = $("<p>").text("Rating: " + rating);
+
+          var animalGif = $("<img>");
+          animalGif.attr("src", results[i].images.fixed_height.url);
+
+          gifDiv.prepend(p);
+          gifDiv.prepend(animalGif);
+
+          $("#gifs").prepend(gifDiv);
+          console.log(response);
+        };
+      });
+
+           function renderButtons() {
+
+        // Deleting the movies prior to adding new movies
+        // (this is necessary otherwise you will have repeat buttons)
+        $("#buttons").empty();
+
+        // Looping through the array of movies
+        for (var i = 0; i < searchButtons.length; i++) {
+
+          // Then dynamicaly generating buttons for each movie in the array
+          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+          var gifButtons = $("<button>");
+          // Adding a class of movie to our button
+          gifButtons.addClass("movie");
+          // Adding a data-attribute
+          gifButtons.attr("data-name", searchButtons[i]);
+          // Providing the initial button text
+          gifButtons.text(searchButtons[i]);
+          // Adding the button to the buttons-view div
+          $("#buttons").append(a);
+        }
+
+        };
+
+      
 
     });
 
 
 
 
-function renderButtons() {
-    $("#buttonDisplay").empty();
-
-    for(i = 0; i < searchArray.length; i++){
-      var button = $("<button>");
-      button.addClass("buttonClass");
-      button.attr("search-term", searchArray[i]);
-      button.text(searchArray[i]);
-      $("#buttonDisplay").append(button);
-    };
-  };
-  });
